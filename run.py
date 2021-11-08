ae351f2c24de82a91b7e7b1bb80182cc687f6b7e
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-08 16:30:01 +0800

    Add naive search function/home/julia/anaconda3/bin/python

--- run.py
+++ run.py
@@ -4,17 +4,25 @@ Comparing with naive search
 """
 
 def naive_search(list_to_search, target):
+    """
+    Naive search function
+    """
     for i in range(len(list_to_search)):
         if list_to_search[i] == target:
             return i
     return -1
 
 def binary_search(list_to_search, target, low=None, high=None):
-    
+    """
+    Binary search function
+    """
     if low is None:
         low = 0
     if high is None:
         high = len(list_to_search) - 1
+
+    if high < low:
+        return -1
     
     mid_point = (len(list_to_search)) // 2
 
@@ -24,3 +32,10 @@ def binary_search(list_to_search, target, low=None, high=None):
         return binary_search(list_to_search, target, low, mid_point-1)
     else:
         return binary_search(list_to_search, target, mid_point+1, high)
+
+
+if __name__ == '__name__':
+    list1 = [1, 3, 5, 6, 7, 9, 11, 13, 15]
+    target1 = 10
+    print(naive_search(list1, target1))
+    print(binary_search(list1, target1))
