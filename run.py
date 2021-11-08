54c88299709e15c1f18f21556109170ec54546ac
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-08 16:48:14 +0800

    Fix bug in the binary search function

--- run.py
+++ run.py
@@ -3,6 +3,10 @@ Binary search implementation
 Comparing with naive search
 """
 
+import random
+import time
+
+
 def naive_search(list_to_search, target):
     """
     Naive search function
@@ -24,7 +28,7 @@ def binary_search(list_to_search, target, low=None, high=None):
     if high < low:
         return -1
     
-    mid_point = (len(list_to_search)) // 2
+    mid_point = (low + high) // 2
 
     if list_to_search == target:
         return mid_point
@@ -35,7 +39,26 @@ def binary_search(list_to_search, target, low=None, high=None):
 
 
 if __name__ == '__main__':
-    list1 = [1, 3, 5, 6, 7, 9, 11, 13, 15, 20]
-    target1 = 9
-    print(naive_search(list1, target1))
-    print(binary_search(list1, target1))
+    # list1 = [1, 3, 5, 6, 7, 9, 11, 13, 15, 20]
+    # target1 = 9
+    # print(naive_search(list1, target1))
+    # print(binary_search(list1, target1))
+
+    length = 10000
+    sorted_list = set()
+    while len(sorted_list) < length:
+        sorted_list.add(random.randint(-3*length, 3*length))
+
+    sorted_list = sorted(list(sorted_list))
+
+    start = time.time()
+    for target in sorted_list:
+        naive_search(sorted_list, target)
+    end = time.time()
+    print(f'Naive search {end - start/length} sec')
+
+    start = time.time()
+    for target in sorted_list:
+        binary_search(sorted_list, target)
+    end = time.time()
+    print(f'Binary search {end - start/length} sec')
