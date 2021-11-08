5cd60b6ada0e9f7a6be6e67ff3d409c0d8d74e53
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-08 16:28:06 +0800

    Add vinary search function

--- run.py
+++ run.py
@@ -9,3 +9,18 @@ def naive_search(list_to_search, target):
             return i
     return -1
 
+def binary_search(list_to_search, target, low=None, high=None):
+    
+    if low is None:
+        low = 0
+    if high is None:
+        high = len(list_to_search) - 1
+    
+    mid_point = (len(list_to_search)) // 2
+
+    if list_to_search == target:
+        return mid_point
+    elif target < list_to_search[mid_point]:
+        return binary_search(list_to_search, target, low, mid_point-1)
+    else:
+        return binary_search(list_to_search, target, mid_point+1, high)
